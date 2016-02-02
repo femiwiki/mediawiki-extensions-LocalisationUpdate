d580ee872ee34f9601c5d1d4f88e1c356df222e0
Niklas Laxström <niklas.laxstrom@gmail.com>
2016-02-02 14:55:41 +0100

    Add phpcs
    Moved things to namespace because LU_ prefix does not pass sniffs.
    
    Change-Id: I6997424d531a9bb4cd767747c1b67dc9cc3138c8

--- LocalisationUpdate.php
+++ LocalisationUpdate.php
@@ -5,7 +5,8 @@ if ( function_exists( 'wfLoadExtension' ) ) {
 	// Keep i18n globals so mergeMessageFileList.php doesn't break
 	$GLOBALS['wgMessagesDirs']['LocalisationUpdate'] = __DIR__ . '/i18n';
 	/* wfWarn(
-		'Deprecated PHP entry point used for LocalisationUpdate extension. Please use wfLoadExtension instead, ' .
+		'Deprecated PHP entry point used for LocalisationUpdate extension. ' .
+		'Please use wfLoadExtension instead, ' .
 		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
 	); */
 	return;
--- update.php
+++ update.php
@@ -24,7 +24,10 @@ class LU extends Maintenance {
 		ini_set( "max_execution_time", 0 );
 		ini_set( 'memory_limit', -1 );
 
-		global $wgExtensionMessagesFiles, $IP;
+		// @codingStandardsIgnoreStart Ignore MediaWiki.NamingConventions.ValidGlobalName.wgPrefix
+		global $IP;
+		// @codingStandardsIgnoreEnd
+		global $wgExtensionMessagesFiles;
 		global $wgLocalisationUpdateRepositories;
 		global $wgLocalisationUpdateRepository;
 
@@ -37,9 +40,9 @@ class LU extends Maintenance {
 		$lc = Language::getLocalisationCache();
 		$messagesDirs = $lc->getMessagesDirs();
 
-		$finder = new LU_Finder( $wgExtensionMessagesFiles, $messagesDirs, $IP );
-		$readerFactory = new LU_ReaderFactory();
-		$fetcherFactory = new LU_FetcherFactory();
+		$finder = new LocalisationUpdate\Finder( $wgExtensionMessagesFiles, $messagesDirs, $IP );
+		$readerFactory = new LocalisationUpdate\ReaderFactory();
+		$fetcherFactory = new LocalisationUpdate\FetcherFactory();
 
 		$repoid = $this->getOption( 'repoid', $wgLocalisationUpdateRepository );
 		if ( !isset( $wgLocalisationUpdateRepositories[$repoid] ) ) {
@@ -50,7 +53,7 @@ class LU extends Maintenance {
 		$repos = $wgLocalisationUpdateRepositories[$repoid];
 
 		// Do it ;)
-		$updater = new LU_Updater();
+		$updater = new LocalisationUpdate\Updater();
 		$updatedMessages = $updater->execute(
 			$finder,
 			$readerFactory,
