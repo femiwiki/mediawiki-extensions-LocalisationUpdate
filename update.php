60e916ac65c9a4b7c8b9f460f29964327ccfb77a
Umherirrender <umherirrender_de.wp@web.de>
2018-03-27 21:28:00 +0200

    phpcs: Use ignoreList of ValidGlobalNameSniff for $IP
    Change-Id: I2b26da9ea9d0702a2b8c5783192fc2abd8d68bf8

--- update.php
+++ update.php
@@ -26,9 +26,7 @@ class Update extends Maintenance {
 		ini_set( "max_execution_time", 0 );
 		ini_set( 'memory_limit', -1 );
 
-		// @codingStandardsIgnoreStart Ignore MediaWiki.NamingConventions.ValidGlobalName.wgPrefix
 		global $IP;
-		// @codingStandardsIgnoreEnd
 		global $wgExtensionMessagesFiles;
 		global $wgLocalisationUpdateRepositories;
 		global $wgLocalisationUpdateRepository;
